42727aca8022c00c9ef2ce4260ffc026c7d8d3a0
Danielyan86 <516495459@qq.com>
2018-10-09 14:52:33 +0800

    complete model part

--- Django_polls/mysite/settings.py
+++ Django_polls/mysite/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',  # install polls
+    'polls.apps.PollsConfig',  # register polls
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ Scripts_tool/write_data_into_DB.py
@@ -0,0 +1,13 @@
+import sqlite3
+import time
+
+current = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+conn = sqlite3.connect('../Django_polls/db.sqlite3')
+c = conn.cursor()
+c.execute("INSERT INTO polls_question "
+          "(question_text, pub_date) "
+          "VALUES ('what is your name','{0}')".format(str(current)))
+
+
+
+conn.commit()

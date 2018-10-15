4111b141b653b78da8e657a43f9fd09252e3ab62
Danielyan86 <516495459@qq.com>
2018-10-15 15:11:27 +0800

    complete part4

--- Scripts_tool/write_data_into_DB.py
+++ Scripts_tool/write_data_into_DB.py
@@ -1,12 +1,29 @@
 import sqlite3
 import time
 
-current = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-conn = sqlite3.connect('../Django_polls/db.sqlite3')
-c = conn.cursor()
-c.execute("INSERT INTO polls_question "
-          "(question_text, pub_date) "
-          "VALUES ('what is your name','{0}')".format(str(current)))
+
+class write_data_into_db():
+    current = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    conn = sqlite3.connect('../Django_polls/db.sqlite3')
+    c = conn.cursor()
+
+    def write_questions(self):
+        self.c.execute("INSERT INTO polls_question "
+                       "(question_text, pub_date) "
+                       "VALUES ('what is your name','{0}')".format(str(self.current)))
+
+        self.conn.commit()
+        self.conn.close()
+
+    def write_choice(self, answer, question_id, ):
+        self.c.execute("INSERT INTO polls_choice "
+                       "(choice_text, votes, question_id) "
+                       "VALUES ('{0}',0,{1})".format(answer, question_id))
+
+        self.conn.commit()
+        self.conn.close()
 
 
-conn.commit()
+if __name__ == '__main__':
+    write_obj = write_data_into_db()
+    write_obj.write_choice("方丈", 7)

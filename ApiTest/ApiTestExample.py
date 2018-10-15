97c473077aa552666f9d669719ead724a81e47df
Danielyan86 <516495459@qq.com>
2018-10-15 17:11:36 +0800

    add api automation test case

---
+++ ApiTest/ApiTestExample.py
@@ -0,0 +1,8 @@
+import requests
+
+url = "http://127.0.0.1:8000/users/"
+req = requests.Session()
+res = req.get(url=url)
+print(res.json())
+res_json = res.json()
+assert res_json[0]['username'] == "xiaodong.yan"

622106e2ba4886c9dcf7e8b67e1c83adf9ba29fb
danielyan86 <516495459@qq.com>
2019-04-14 11:34:11 +0800

    update

--- Scripts_tool/write_dataIntoDb.py
+++ Scripts_tool/write_dataIntoDb.py
@@ -4,9 +4,10 @@ import time
 
 class WriteDataIntoDb:
     current = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    conn = sqlite3.connect('../Django_polls/db.sqlite3')
+    conn = sqlite3.connect('../Django_polls/db.sqlite3')  # 连接数据库
     c = conn.cursor()
 
+    # 通过sql语句写入直接
     def write_questions(self):
         self.c.execute("INSERT INTO polls_question "
                        "(question_text, pub_date) "

818893622c280c7d3ab7babac023e500c42ca361
Danielyan86 <516495459@qq.com>
2020-10-24 22:11:53 +0800

    update

--- Scripts_tool/write_dataIntoDb.py
+++ Scripts_tool/write_dataIntoDb.py
@@ -8,13 +8,18 @@ class WriteDataIntoDb:
     c = conn.cursor()
 
     # 通过sql语句写入直接
-    def write_questions(self):
+    def write_questions(self, question_text):
+        current_time = self.current
         self.c.execute("INSERT INTO polls_question "
                        "(question_text, pub_date) "
-                       "VALUES ('what is your name','{0}')".format(str(self.current)))
+                       f"VALUES ('{question_text}','{current_time}')")
 
         self.conn.commit()
-        self.conn.close()
+
+    def get_question_id(self, question_text):
+        self.c.execute("SELECT id FROM polls_question WHERE question_text = '%s'" % question_text)
+        res = self.c.fetchall()
+        return res[0][0]
 
     def write_choice(self, answer, question_id, ):
         self.c.execute("INSERT INTO polls_choice "
@@ -22,9 +27,14 @@ class WriteDataIntoDb:
                        "VALUES ('{0}',0,{1})".format(answer, question_id))
 
         self.conn.commit()
+
+
+    def close_db(self):
         self.conn.close()
 
 
 if __name__ == '__main__':
     write_obj = WriteDataIntoDb()
-    write_obj.write_choice("小李子", 2)  # 第二个参数是问题的id
+    # write_obj.write_questions("谁是天下第一？")
+    # id = write_obj.get_qustion_id("谁是天下第一？")
+    write_obj.write_choice("小李子", 3)  # 第二个参数是问题的id
